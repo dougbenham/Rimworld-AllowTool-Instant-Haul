d7cf36fd403f8a275ca6d420e60dd0cd34b6d94d
Doug Benham <dougbenham@cox.net>
2021-02-03 02:06:09 -0700

    Initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# AllowTool - Instant Haul
+If god-mode is enabled, using 'haul urgently' will immediately transport the target.
---
+++ Source/Main.cs
@@ -0,0 +1,42 @@
+﻿using AllowTool;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace AllowToolInstantHaul
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            var harmony = new Harmony("doug.allowtool.instanthaul");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(Designator_HaulUrgently), "DesignateThing")]
+    public static class Designator_HaulUrgently_DesignateThing_Patch
+    {
+        [HarmonyPrefix]
+        [HarmonyAfter("UnlimitedHugs.AllowTool")]
+        public static bool HaulInstantly(ref Thing thing)
+        {
+            if (DebugSettings.godMode &&
+                StoreUtility.TryFindBestBetterStoreCellFor(thing, null, thing.Map, StoragePriority.Unstored, Faction.OfPlayer, out var target, true))
+            {
+                Map thingMap = thing.Map; // despawning loses Thing's Map
+
+                if (thing.Spawned)
+                    thing.DeSpawn();
+
+                if (!GenPlace.TryPlaceThing(thing, target, thingMap, ThingPlaceMode.Direct))
+                    thing.Position = target; // forcefully set position if stacking nicely failed
+
+                return false;
+            }
+            
+            return true;
+        }
+    }
+}
